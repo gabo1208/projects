5c6bd255255a1da95235137c13404bfebbc7996e
Gabriel Freites <gabohd@gmail.com>
2025-12-28 23:42:02 +0100

    improved a bit the code quality

--- js/js/tsoding-formula/index.js
+++ js/js/tsoding-formula/index.js
@@ -15,7 +15,7 @@ function clear() {
     ctx.fillRect(0, 0, game.width, game.height)
 }
 
-function point({ x, y, w = 20, h = 20 }) {
+function printRectangle({ x, y, w = 20, h = 20 }) {
     ctx.fillStyle = FOREGROUND
     // the minus size/2 its to avoid the point disappearing over the screen limits
     ctx.fillRect(x - w / 2, y - h / 2, w, h)
@@ -30,18 +30,23 @@ function screen(point) {
     }
 }
 
-//(x,y)|      |(screen)--->
-//     |      |
-//     |      |
-// eye o_ _ _ | _ _ _ _ _ ---> (z projection inside the screen...)
-//    z|   z=1|
+// p1 (small triangle) follows the proportion between similar triangle
+// since it has the same angles as p2 (big triangle), so we can derive the equation:
+// x´ = x/z and y´=y/z since X/x = Y/y = Z/z with z = 1
+//
+//(x,y)|    |(screen)--->
+//     |    |/ | <-- projection in p1 of the point in z to p2
+//     |  / |  |
+// eye o/_p1|p2|_ _ _ _ _ ---> (z projection inside the screen...)
+//    z| z=1|
+//
 // z can´t be zero since it will not project anything since it's at the same
 // level as the "eye". z = 1 its at the screen leven
 //
 //
 // i.e, this two points give the same projection since they are straight from the eye
-// point(screen(project({ x: 0, y: 0, z: 1 })))
-// point(screen(project({ x: 0, y: 0, z: 2 })))
+// printRectangle(screen(project({ x: 0, y: 0, z: 1 })))
+// printRectangle(screen(project({ x: 0, y: 0, z: 2 })))
 function project({ x, y, z }) {
     return {
         x: x / z,
@@ -49,7 +54,7 @@ function project({ x, y, z }) {
     }
 }
 
-function line(p1, p2) {
+function drawLine(p1, p2) {
     ctx.lineWidth = 3
     ctx.strokeStyle = FOREGROUND
     ctx.beginPath()
@@ -63,6 +68,18 @@ function translate_z({ x, y, z }, dz, direction = 1) {
 }
 
 // to rotate in one axis, use the other ones
+// rotate in x
+function rotate_yz({ x, y, z }, angle) {
+    const c = Math.cos(angle)
+    const s = Math.sin(angle)
+    return {
+        x: x,
+        y: y * c - z * s,
+        z: x * s + z * c,
+    }
+}
+
+// rotate in y
 function rotate_xz({ x, y, z }, angle) {
     const c = Math.cos(angle)
     const s = Math.sin(angle)
@@ -70,6 +87,33 @@ function rotate_xz({ x, y, z }, angle) {
         x: x * c - z * s,
         y,
         z: x * s + z * c,
+    }
+}
+
+function transformPoints(point, angle, dz) {
+    // first apply any rotation
+    // then translate z from p2 to p1 (inside the screen to the exact screen point z = 1)
+    // then transform the x and y to the z plane
+    // then normalize the point in the screen canvas dimensions
+    return screen(project((translate_z(rotate_xz(point, angle), dz))))
+}
+
+function printVertices(vertices, angle, dz) {
+    for (const v of vertices) {
+        printRectangle(transformPoints(v, angle, dz))
+    }
+}
+
+function printLines(vertices, faces, angle, dz) {
+    for (const f of faces) {
+        for (let i = 0; i < f.length; ++i) {
+            const a = vertices[f[i]]
+            const b = vertices[f[(i + 1) % f.length]]
+            drawLine(
+                transformPoints(a, angle, dz),
+                transformPoints(b, angle, dz),
+            )
+        }
     }
 }
 
@@ -88,12 +132,12 @@ const vertices = [
 const faces = [
     [0, 1, 2, 3],
     [4, 5, 6, 7],
+
     [0, 4],
     [1, 5],
     [2, 6],
     [3, 7]
 ]
-
 const FPS = 60
 const dt = 1 / FPS // delta time between two frames
 
@@ -105,21 +149,16 @@ let angle = 0
 function frame() {
     dz += 1 * dt // comment if you don't want any projection
     angle += 2 * Math.PI * dt
-    clear()
-    for (const v of vertices) {
-        point(screen(project((translate_z(rotate_xz(v, angle), dz)))))
-    }
 
-    for (const f of faces) {
-        for (let i = 0; i < f.length; ++i) {
-            const a = vertices[f[i]]
-            const b = vertices[f[(i + 1) % f.length]]
-            line(
-                screen(project((translate_z(rotate_xz(a, angle), dz)))),
-                screen(project((translate_z(rotate_xz(b, angle), dz)))),
-            )
-        }
-    }
+    // reset screen
+    clear()
+
+    // print stuff
+    printVertices(vertices, angle, dz)
+    printLines(vertices, faces, angle, dz)
+
+    // callback to next frame
     setTimeout(frame, 1000 / FPS)
 }
+
 setTimeout(frame, 1000 / FPS)
